c5f7b0d803d11d51437a01e3e5149d261fe09db6
preitychib <preeti.chib2015@gmail.com>
2024-08-17 14:50:12 +0530

    test commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,9 +7,13 @@ module.exports = {
     extend: {
       backgroundImage: {
         'primary-bg': 'linear-gradient(135deg, #1e0533, #110a1f)',
+        'primary-bg-light': 'linear-gradient(316deg, #cdc1ff 0%, #e5d9f2 74%)',
       },
       colors: {
         'bright-purple': '#9b5de5', // Replace with your desired shade
+      },
+      fontFamily: {
+        tomatoesfont: ['TomatoesFont', 'sans-serif'], 
       },
     },
   },
